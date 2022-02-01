5b24a213411d73d299453e18547213d5a0146453
spences10 <spences10apps@gmail.com>
2022-02-01 20:11:55 +0000

    feat: :sparkles: init

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,22 @@
+<script>
+  import { browser } from '$app/env'
+  import { page } from '$app/stores'
+  import * as Fathom from 'fathom-client'
+  import { onMount } from 'svelte'
+
+  onMount(async () => {
+    Fathom.load(import.meta.env.VITE_FATHOM_ID, {
+      url: import.meta.env.VITE_FATHOM_URL,
+    })
+  })
+
+  $: $page.pathname, browser && Fathom.trackPageview()
+</script>
+
+<ul>
+  <a href="/" on:click={Fathom.trackGoal(`KWOYX0PK`)}>Home</a>
+  <a href="/about">About</a>
+  <a href="/services">Services</a>
+</ul>
+
+<slot />
---
+++ src/routes/index.svelte
@@ -0,0 +1,15 @@
+<script>
+  import { trackGoal } from 'fathom-client'
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>
+  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the
+  documentation
+</p>
+
+<button on:click={trackGoal(`Q0PJFFBW`, 100)}>
+  Don't Click This Button!!</button
+>
+
+
