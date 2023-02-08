ad292bff22d99b6ca3c616c39a994a987a278756
spences10 <spences10apps@gmail.com>
2023-02-08 20:07:24 +0000

    refactor: :recycle: update nav links
    add index export

---
+++ src/lib/components/index.ts
@@ -0,0 +1,2 @@
+export { default as Nav } from './nav.svelte'
+export { default as PricingCard } from './pricing-card.svelte'
--- src/lib/components/nav.svelte
+++ src/lib/components/nav.svelte
@@ -1,29 +1,39 @@
-<script>
+<script lang="ts">
 	import { trackGoal } from 'fathom-client'
+
+	let links = [
+		{
+			href: '/pricing',
+			text: 'Pricing',
+		},
+		{
+			href: '/contact',
+			text: 'Contact Us',
+		},
+		{
+			href: '/about',
+			text: 'About',
+		},
+		{
+			href: '/blog',
+			text: 'Blog',
+		},
+	]
 </script>
 
-<div
-	class="navbar rounded-box mb-10 bg-neutral text-neutral-content shadow-lg"
->
+<div class="navbar mb-10 bg-neutral text-neutral-content shadow-lg">
 	<div class="navbar-start mx-2 px-2">
-		<a href="/" on:click={trackGoal(`KWOYX0PK`)}>
+		<a href="/" on:click={() => trackGoal(`KWOYX0PK`, 0)}>
 			<span class="text-lg font-bold">SvelteKit and Fathom</span>
 		</a>
 	</div>
 	<div class="navbar-center mx-2 hidden px-2 lg:flex">
 		<div class="flex items-stretch">
-			<a href="/pricing" class="btn-ghost rounded-btn btn-sm btn">
-				Pricing
-			</a>
-			<a href="/contact" class="btn-ghost rounded-btn btn-sm btn">
-				Contact Us
-			</a>
-			<a href="/about" class="btn-ghost rounded-btn btn-sm btn">
-				About
-			</a>
-			<a href="/blog" class="btn-ghost rounded-btn btn-sm btn">
-				Blog
-			</a>
+			{#each links as { href, text }}
+				<a {href} class="btn-ghost rounded-btn btn-sm btn">
+					{text}
+				</a>
+			{/each}
 		</div>
 	</div>
 	<div class="navbar-end" />
